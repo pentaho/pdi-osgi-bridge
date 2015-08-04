a8f03d2a6edb9c39ecdfd62f474308e2684c2eb8
Benjamin Morrise <bmorrise@pentaho.com>
2015-08-04 14:54:05 -0400

    [BACKLOG-3756] Update projects with the pentaho vfs version to version 2

--- activator/src/test/java/org/pentaho/osgi/blueprint/test/TestKettlePlugin.java
+++ activator/src/test/java/org/pentaho/osgi/blueprint/test/TestKettlePlugin.java
@@ -113,7 +113,7 @@ public class TestKettlePlugin {
           bootClasspathLibraries( "mvn:pentaho-kettle/kettle-core/" + projectVersion,
               "mvn:pentaho-kettle/kettle-engine/" + projectVersion
               , "mvn:pentaho/pentaho-osgi-utils-api/" + projectVersion
-              , "mvn:commons-vfs/commons-vfs/1.0",
+              , "mvn:org.apache.commons/commons-vfs2/2.0",
               "mvn:net.sf.scannotation/scannotation/1.0.2" ),
 //          vmOption( "-Xrunjdwp:transport=dt_socket,server=y,suspend=y,address=5005" ),
           systemPackages( "org.pentaho.di.i18n", "org.pentaho.osgi.api", "org.pentaho.di.core.plugins",

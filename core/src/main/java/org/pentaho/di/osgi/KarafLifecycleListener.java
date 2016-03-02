a060be42b8f94d061fcbd49e54e0a1d62374bd4a
brosander <brosander@pentaho.com>
2016-03-02 11:37:56 -0500

    [BACKLOG-6886] - Waiting for delayed notifiers to process before accepting event

--- core/src/main/java/org/pentaho/di/osgi/KarafLifecycleListener.java
+++ core/src/main/java/org/pentaho/di/osgi/KarafLifecycleListener.java
@@ -1,8 +1,33 @@
+/*! ******************************************************************************
+ *
+ * Pentaho Data Integration
+ *
+ * Copyright (C) 2002-2016 by Pentaho : http://www.pentaho.com
+ *
+ *******************************************************************************
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with
+ * the License. You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ ******************************************************************************/
+
 package org.pentaho.di.osgi;
 
+import com.google.common.annotations.VisibleForTesting;
 import org.osgi.framework.BundleContext;
 import org.osgi.framework.ServiceReference;
 import org.pentaho.di.core.util.ExecutorUtil;
+import org.pentaho.di.osgi.service.lifecycle.LifecycleEvent;
+import org.pentaho.di.osgi.service.notifier.DelayedServiceNotifierListener;
 import org.pentaho.osgi.api.IKarafBlueprintWatcher;
 import org.pentaho.osgi.api.IKarafFeatureWatcher;
 import org.pentaho.platform.servicecoordination.api.IPhasedLifecycleEvent;
@@ -22,17 +47,26 @@ public class KarafLifecycleListener implements IPhasedLifecycleListener<KettleLi
   private static KarafLifecycleListener INSTANCE = new KarafLifecycleListener( );
   private static Logger logger = LoggerFactory.getLogger( KarafLifecycleListener.class );
   private final long timeout;
+  private final OSGIPluginTracker osgiPluginTracker;
   private AtomicBoolean listenerActive = new AtomicBoolean( false );
   private AtomicBoolean initialized = new AtomicBoolean( false );
   private BundleContext bundleContext;
   private IPhasedLifecycleEvent<KettleLifecycleEvent> event;
 
+  @VisibleForTesting
   KarafLifecycleListener() {
     this( calculateTimeout() );
   }
 
+  @VisibleForTesting
   KarafLifecycleListener( long timeout ) {
+    this( timeout, OSGIPluginTracker.getInstance() );
+  }
+
+  @VisibleForTesting
+  KarafLifecycleListener( long timeout, OSGIPluginTracker osgiPluginTracker ) {
     this.timeout = timeout;
+    this.osgiPluginTracker = osgiPluginTracker;
   }
 
   private static long calculateTimeout() {
@@ -84,8 +118,8 @@ public class KarafLifecycleListener implements IPhasedLifecycleListener<KettleLi
         if ( !initialized.get() ) {
           // We fell out due to time or an exception. Ensure that we release the lifecycle hold
           logger.error(
-              "The Kettle Karaf Lifecycle Listener failed to execute properly after waiting for " + TimeUnit.MILLISECONDS.toSeconds(timeout)
-                      + " seconds. Releasing lifecycle hold, but some services may be unavailable." );
+            "The Kettle Karaf Lifecycle Listener failed to execute properly after waiting for " + TimeUnit.MILLISECONDS
+              .toSeconds( timeout ) + " seconds. Releasing lifecycle hold, but some services may be unavailable." );
           event.accept();
         }
       }
@@ -129,13 +163,25 @@ public class KarafLifecycleListener implements IPhasedLifecycleListener<KettleLi
             logger.error( "Error in Blueprint Watcher", e );
           }
 
-          event.accept();
+          final AtomicBoolean accepted = new AtomicBoolean( false );
+          DelayedServiceNotifierListener delayedServiceNotifierListener = new DelayedServiceNotifierListener() {
+            @Override public void onRun( LifecycleEvent lifecycleEvent, Object serviceObject ) {
+              if ( osgiPluginTracker.getOutstandingServiceNotifierListeners() == 0 && !accepted.getAndSet( true ) ) {
+                logger.debug( "Done waiting on delayed service notifiers" );
+                event.accept();
+                osgiPluginTracker.removeDelayedServiceNotifierListener( this );
+              }
+            }
+          };
+
+          logger.debug( "About to start waiting on delayed service notifiers" );
+          osgiPluginTracker.addDelayedServiceNotifierListener( delayedServiceNotifierListener );
+          delayedServiceNotifierListener.onRun( null, null );
         }
       } );
       thread.setDaemon( true );
       thread.start();
       initialized.set( true );
-
     }
   }
 
--- core/src/test/java/org/pentaho/di/osgi/KarafLifecycleListenerTest.java
+++ core/src/test/java/org/pentaho/di/osgi/KarafLifecycleListenerTest.java
@@ -1,23 +1,80 @@
+/*! ******************************************************************************
+ *
+ * Pentaho Data Integration
+ *
+ * Copyright (C) 2002-2016 by Pentaho : http://www.pentaho.com
+ *
+ *******************************************************************************
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with
+ * the License. You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ ******************************************************************************/
+
 package org.pentaho.di.osgi;
 
+import org.junit.Before;
 import org.junit.Test;
+import org.mockito.ArgumentCaptor;
 import org.osgi.framework.BundleContext;
 import org.osgi.framework.ServiceReference;
+import org.pentaho.di.osgi.service.notifier.DelayedServiceNotifierListener;
 import org.pentaho.osgi.api.IKarafBlueprintWatcher;
 import org.pentaho.osgi.api.IKarafFeatureWatcher;
 import org.pentaho.platform.servicecoordination.api.IPhasedLifecycleEvent;
+import org.pentaho.platform.servicecoordination.api.IPhasedLifecycleManager;
 import org.pentaho.platform.servicecoordination.impl.BaseCountdownLatchLifecycleManager;
 
+import java.util.concurrent.TimeUnit;
 import java.util.concurrent.atomic.AtomicBoolean;
 
 import static org.junit.Assert.*;
 import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.never;
+import static org.mockito.Mockito.timeout;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
 /**
  * Created by nbaker on 9/23/15.
  */
 public class KarafLifecycleListenerTest {
+  private int timeout;
+  private OSGIPluginTracker osgiPluginTracker;
+  private KarafLifecycleListener karafLifecycleListener;
+  private BundleContext bundleContext;
+  private ServiceReference<IKarafFeatureWatcher> featureWatcherServiceReference;
+  private IKarafFeatureWatcher iKarafFeatureWatcher;
+  private ServiceReference<IKarafBlueprintWatcher> blueprintWatcherServiceReference;
+  private IKarafBlueprintWatcher iKarafBlueprintWatcher;
+
+  @Before
+  public void setup() {
+    timeout = 500;
+    osgiPluginTracker = mock( OSGIPluginTracker.class );
+    karafLifecycleListener = new KarafLifecycleListener( timeout, osgiPluginTracker );
+
+    bundleContext = mock( BundleContext.class );
+    karafLifecycleListener.setBundleContext( bundleContext );
+
+    featureWatcherServiceReference = mock( ServiceReference.class );
+    iKarafFeatureWatcher = mock( IKarafFeatureWatcher.class );
+    when( bundleContext.getService( featureWatcherServiceReference ) ).thenReturn( iKarafFeatureWatcher );
+
+    blueprintWatcherServiceReference = mock( ServiceReference.class );
+    iKarafBlueprintWatcher = mock( IKarafBlueprintWatcher.class );
+    when( bundleContext.getService( blueprintWatcherServiceReference ) ).thenReturn( iKarafBlueprintWatcher );
+  }
 
   @Test
   public void testSatisfied() throws Exception {
@@ -110,5 +167,25 @@ public class KarafLifecycleListenerTest {
     // sure that happens
     t.join();
     assertEquals( 2, lifecycleManager.getPhase() );
+  }
+
+  @Test
+  public void testDelayedServiceNotifierListener() {
+    IPhasedLifecycleEvent<KettleLifecycleEvent> iPhasedLifecycleEvent = mock( IPhasedLifecycleEvent.class );
+    when( iPhasedLifecycleEvent.getNotificationObject() ).thenReturn( KettleLifecycleEvent.INIT );
+
+    when( bundleContext.getServiceReference( IKarafFeatureWatcher.class ) ).thenReturn( featureWatcherServiceReference );
+    when( bundleContext.getServiceReference( IKarafBlueprintWatcher.class ) ).thenReturn( blueprintWatcherServiceReference );
+
+    when( osgiPluginTracker.getOutstandingServiceNotifierListeners() ).thenReturn( 1 ).thenReturn( 0 );
+    karafLifecycleListener.onPhaseChange( iPhasedLifecycleEvent );
+    ArgumentCaptor<DelayedServiceNotifierListener> delayedServiceNotifierListenerArgumentCaptor =
+      ArgumentCaptor.forClass( DelayedServiceNotifierListener.class );
+    verify( osgiPluginTracker, timeout( (int) TimeUnit.SECONDS.toMillis( 5 ) ) ).addDelayedServiceNotifierListener( delayedServiceNotifierListenerArgumentCaptor.capture() );
+    verify( iPhasedLifecycleEvent, never() ).accept();
+    delayedServiceNotifierListenerArgumentCaptor.getValue().onRun( null, null );
+    verify( iPhasedLifecycleEvent ).accept();
+    delayedServiceNotifierListenerArgumentCaptor.getValue().onRun( null, null );
+    verify( iPhasedLifecycleEvent, times( 1 ) ).accept();
   }
 }
